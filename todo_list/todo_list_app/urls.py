5115c2fb3d1c55471d2f077dee013b7cc1e49096
M-Muaz-Shoukat <muaz.shoukat@codefulcrum.com>
2024-05-21 14:22:27 +0500

    restructures UI + added search option

--- todo_list/todo_list_app/urls.py
+++ todo_list/todo_list_app/urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 from . import views
 app_name = 'todo_list'
 urlpatterns = [
-    path('categories/', views.index, name='all_categories'),
+    path('', views.index, name='index'),
+    path('categories/', views.all_categories, name='all_categories'),
     path('category/create', views.create_category, name='create_category'),
     path('category/<int:category_id>/update', views.update_category, name='category_update'),
     path('category/<int:category_id>/delete', views.delete_category, name='category_delete'),
-    path('category/<int:category_id>/tasks', views.category_tasks, name='category_tasks'),
-    path('category/<int:category_id>/tasks/create', views.category_task_create, name='category_task_create'),
-    path('category/<int:category_id>/tasks/<int:task_id>/delete', views.category_task_delete, name='category_task_delete'),
-    path('category/<int:category_id>/tasks/<int:task_id>/update', views.category_task_update, name='category_task_update'),
-    path('auth/login', views.login_user, name='auth_login'),
+    path('tasks/', views.tasks, name='tasks'),
+    path('tasks/create', views.task_create, name='task_create'),
+    path('tasks/<int:task_id>/delete', views.task_delete, name='task_delete'),
+    path('tasks/<int:task_id>/update', views.task_update, name='task_update'),
+    # path('auth/login', views.login_user, name='auth_login'),
 ]
--- todo_list/todo_list_app/views.py
+++ todo_list/todo_list_app/views.py
@@ -1,17 +1,26 @@
 from django.shortcuts import render, redirect
 from .models import Category, Task
+from django.db.models import Q
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django import forms
-# Create your views here.
 
 
 def login_user(request):
-    return  render(request, 'auth/login.html',{})
+    return render(request, 'auth/login.html', {})
+
 
 def index(request):
+    return render(request, 'todo_list_app/index.html')
+
+
+def all_categories(request):
+    query = request.GET.get('q')
     category_list = Category.objects.all()
-    return render(request, 'todo_list_app/index.html', {'category_list': category_list})
+
+    if query:
+        category_list = category_list.filter(name__icontains=query)
+    return render(request, 'todo_list_app/all_categories.html', {'category_list': category_list,'query':query})
 
 
 def delete_category(request, category_id):
@@ -52,42 +61,51 @@ class CategoryForm(forms.ModelForm):
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ['title', 'description', 'due_date', 'completed']
+        fields = ['title', 'description', 'due_date', 'completed','category']
 
 
-def category_tasks(request, category_id):
-    category = Category.objects.get(id=category_id)
-    task_list = Task.objects.filter(category_id=category_id)
-    return render(request, 'todo_list_app/category_tasks.html', {'task_list': task_list, 'category': category})
+def tasks(request):
+    query = request.GET.get('q')
+    task_list = Task.objects.all()
+    if query:
+        task_list = task_list.filter(
+            Q(title__icontains=query) |
+            Q(description__icontains=query) |
+            Q(category__name__icontains=query))
+    return render(request, 'todo_list_app/tasks.html', {
+        'task_list': task_list,
+        'query': query
+    })
 
 
-def category_task_delete(request,category_id, task_id):
+def task_delete(request, task_id):
     Task.objects.get(id=task_id).delete()
-    return redirect('todo_list:category_tasks', category_id)
+    return redirect('todo_list:tasks')
 
 
-def category_task_create(request, category_id):
+def task_create(request):
+    categories = Category.objects.all()
     if request.method == 'POST':
         form = TaskForm(request.POST)
         if form.is_valid():
-            task = form.save(commit=False)
-            task.category_id = category_id
-            task.save()
-            return redirect('todo_list:category_tasks',category_id=category_id)
+            form.save()
+            return redirect('todo_list:tasks')
     else:
         form = TaskForm()
-        return render(request, 'todo_list_app/category_task_create.html', {'form': form})
+        return render(request, 'todo_list_app/task_create.html', {'form': form,'categories': categories})
 
 
-def category_task_update(request, category_id, task_id):
+def task_update(request, task_id):
     task = Task.objects.get(id=task_id)
+    categories = Category.objects.all()
     if request.method == 'POST':
-        task.title = request.POST.get('title')
-        task.description = request.POST.get('description')
-        task.due_date = request.POST.get('due_date')
-        task.completed = bool(request.POST.get('completed'))
-        task.save()
-        return redirect('todo_list:category_tasks', category_id=category_id)
+        form = TaskForm(request.POST,instance=task)
+        if form.is_valid():
+            form.save()
+        return redirect('todo_list:tasks')
     else:
-        return render(request, 'todo_list_app/category_task_update.html', {'category_id': category_id, 'task': task})
-
+        form = TaskForm()
+        return render(request,
+                      'todo_list_app/task_update.html',
+                      {'task':
+                          task, 'categories': categories,'form': form})
